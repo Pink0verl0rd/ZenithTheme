701d34662911466ef9683bc3ed39a88be9c74be7
Jun Ivar Doydora <ivardoydora@gmail.com>
2023-08-16 12:49:53 -0400

    added functions and screenshot

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compat   ible" content="ie=edge">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
     <title>Wordpress Theme</title>
 </head>
