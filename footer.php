6571246c6869b6cf717b1da577522fd555ec0039
Jun Ivar Doydora <ivardoydora@gmail.com>
2023-08-16 12:12:55 -0400

    header and footer init

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Footer template.
+ * 
+ * @package Zenith
+ */
+?>
+
+
+<footer>This is the header</footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,17 +5,12 @@
  * @package Zenith
  */
 
- ?>
- 
- <!doctype html>
- <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Wordpress Theme</title>
-    </head>
-    <body>
-        Hello World
-    </body>
- </html>
+get_header();
+
+?>
+
+<div class="content">Hello World</div>
+
+<?php 
+
+get_footer();
