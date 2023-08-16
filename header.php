0cc0c7aec340b15c24144c72afa1389c9a9aa13b
Jun Ivar Doydora <ivardoydora@gmail.com>
2023-08-16 12:26:32 -0400

    wp_body & body class

--- header.php
+++ header.php
@@ -7,14 +7,15 @@
 ?>
 
 <!doctype html>
-<html lang="<?php language_attributes();?>">
+<html lang="<?php language_attributes(); ?>">
 <head>
-    <meta charset="<?php bloginfo('charset');?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compat   ible" content="ie=edge">
     <?php wp_head(); ?>
     <title>Wordpress Theme</title>
 </head>
-<body>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
 <header>This is the Header</header>
