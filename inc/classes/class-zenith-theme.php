6957ed53515584ab8246c04821c18bf3e9caf381
Jun Ivar Doydora <ivardoydora@gmail.com>
2023-08-17 14:12:57 -0400

    More Theme Supports

--- inc/classes/class-zenith-theme.php
+++ inc/classes/class-zenith-theme.php
@@ -29,6 +29,8 @@ class ZENITH_THEME {
     }
 
     public function setup_theme(){
+        
+
         add_theme_support('title-tag'); // Expects WordPress to provide the title tag instead
         add_theme_support('custom-logo', [
             'height'               => 100,
@@ -37,6 +39,32 @@ class ZENITH_THEME {
             'flex-width'           => true,
             'header-text'          => [ 'site-title', 'site-description' ],
             'unlink-homepage-logo' => true, 
-        ]); // Expects WordPress to provide the title tag instead
-    }
+        ]); // Logo
+        add_theme_support('custom-background',[
+            'default-color'      => '#fff',
+            'default-image'      => '',
+
+        ]);
+        add_theme_support('post-thumbnails');
+        add_theme_support('customize-selective-refresh-widgets');
+        add_theme_support('automatic-feed-links');   
+        add_theme_support( 'html5', [
+            'comment-list',
+            'comment-form',
+            'search-form',
+            'gallery',
+            'caption',
+            'style',
+            'script' 
+        ]);
+        add_editor_style(); //  Includes the Editor style sheet
+        add_theme_support('wp-block-styles');
+        add_theme_support('align-wide');
+
+        // Max width will be set all over the place 
+        global $content_width;
+        if ( !isset($content_width)){
+            $content_width = 1240;
+        }
+    }   
 }
