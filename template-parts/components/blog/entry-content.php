47c8f5b5fd95d8f32c9ce471ed723933a8b58ea8
Jun Ivar Doydora <ivardoydora@gmail.com>
2023-08-24 11:19:18 -0400

    custom excerpt

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -2,11 +2,33 @@
 /**
  * Content Template
  * 
- * @package zenith
+ * To be used in Wordpress The Loop
  * 
+ * @package zenith
  */
 
 ?>
 
 
-<div> <?php the_excerpt();?></div>
+<div class="entry-content">
+    <?php
+    if ( is_single() ) {
+        the_content( 
+            sprintf(
+                wp_kses( // We do kses for security 
+                    __('Continue reading %s <span class="meta-nav">&rarr;</span>','zenith'),
+                    [
+                        'span' => [
+                            'class' => []
+                        ]
+                    ]
+                ),
+                the_title( '<span class="screen-reader-text">"', '"</span>',false)
+            )
+        );
+    }
+    else {
+        zenith_the_excerpt(200);
+    }
+    ?>
+</div>
